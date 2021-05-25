93a70dfd102b633cae67a33fba4ce291f97b0aff
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-25 13:50:23 -0500

    tried doing a route a different way (in server.js)

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require('./routes');
+// const routes = require('./routes');
+const db = require("./models");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -11,10 +12,20 @@ app.use(express.json());
 
 app.use(logger("dev"));
 
-app.use(routes);
+// app.use(routes);
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true,});
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
