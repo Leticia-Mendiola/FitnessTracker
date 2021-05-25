13de422762e53236b77bcd06f601784116652e97
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-24 23:56:20 -0500

    changed PORT number out of personal preference

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require('./routes');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
