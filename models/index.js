bbaa5ed740deba16909fdad07af61b7fca34ec77
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-24 21:55:01 -0500

    renamed schema & model to 'Workout'

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const ExerciseSchema = new Schema({
+const WorkoutSchema = new Schema({
     type: {
         type: String,
         trim: true,
@@ -30,6 +30,6 @@ const ExerciseSchema = new Schema({
     }
 });
 
-const Exercise = mongoose.model("Exercise", ExerciseSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports = Exercise;
+module.exports = Workout;
