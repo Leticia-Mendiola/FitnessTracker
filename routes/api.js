2f31a5265adbf842e797ef0f0bc443e8d82cf45b
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-25 16:30:31 -0500

    tried putting useFindAndModify back into mongoose.connect syntax

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true,});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false});
 
 router.get('/workouts', (req,res) => {
     db.Workout.find({})
--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true,});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
