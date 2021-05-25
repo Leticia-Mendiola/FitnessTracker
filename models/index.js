54cd5c47325a1a1c200490d764eec1b164e17cd7
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-24 22:53:15 -0500

    made models/index.js more like a traditional index.js which exports workout.js

--- models/index.js
+++ models/index.js
@@ -1,35 +1,3 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true,
-        required: "Workout Type is Required"
-    },
-    name: {
-        type: String,
-        trim: true,
-        required: "Exercise Name is Required"
-    },
-    duration: {
-        type: Number,
-    },
-    weight: {
-        type: Number,
-    },
-    reps: {
-        type: Number,
-    },
-    sets: {
-        type: Number,
-    },
-    distance: {
-        type: Number,
-    }
-});
-
-const Workout = mongoose.model("Workout", WorkoutSchema);
-
-module.exports = Workout;
+module.exports = {
+    Workout: require("./Workout"),
+  };
