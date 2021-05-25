42f16c4716d49ef914dd256173cbd9541d4bc553
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-24 23:33:59 -0500

    modified routes handling in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const routes = require('./routes/index.js');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -8,15 +9,15 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(logger("dev"));
+
+app.use(routes);
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/index.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
