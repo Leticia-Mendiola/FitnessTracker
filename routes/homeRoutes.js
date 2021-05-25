09256d2e147d8502a91c145b8cfa4497485aa194
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-25 00:08:23 -0500

    fixed __dirname syntax in routes NOW ABLE TO ACTUALLY VIEW WEBSITE

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const path = require('path');
 
 // routes to send users to correct html page
-router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, 'exercise.html')));
-router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, 'stats.html')));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public/index.html')));
+router.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '..', 'public/exercise.html')));
+router.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '..', 'public/stats.html')));
 
 module.exports = router;
