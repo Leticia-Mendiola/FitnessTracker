00872e1e78b94ab6526cbc7aee799f71b3bb5eaa
Leticia-Mendiola <lmendiola509@gmail.com>
2021-05-25 13:43:48 -0500

    even further simplified mongoose.connect

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ app.use(logger("dev"));
 app.use(routes);
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true,});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
